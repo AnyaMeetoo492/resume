8e7f6c073a437f1a45bbaa66a2d7c23d98e76aca
Anya Meetoo <108736938+AnyaMeetoo492@users.noreply.github.com>
2026-05-21 13:50:34 +0200

    Fix export path for Volunteering component

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,5 +8,5 @@ export { WorkExperience } from "./WorkExperience";
 export { Publications } from "./Publications";
 export { EducationSection } from "./EducationSection";
 export { Skills } from "./Skills";
-export { Volunteering } from "./Volunteering";
+export { Volunteering } from "./Volunteerings";
 export { Section } from "./Section";
