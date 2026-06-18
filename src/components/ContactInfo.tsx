6da1f11b1f9c4212214cde64c0275609211bdac6
Anya Meetoo <anyameetoo@gmail.com>
2026-06-18 15:47:25 +0200

    Create ContactInfo.tsx

---
+++ src/components/ContactInfo.tsx
@@ -0,0 +1,13 @@
+interface ContactInfoProps {
+  icon: React.ReactNode;
+  children: React.ReactNode;
+}
+
+export function ContactInfo({ icon, children }: ContactInfoProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <span>{icon}</span>
+      <span>{children}</span>
+    </div>
+  );
+}
